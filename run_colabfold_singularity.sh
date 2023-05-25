b7b38fbc8c6b048a49e902a0925d6a5c79cccb99
James Abbott <j.abbott@dundee.ac.uk>
2023-05-25 17:14:28 +0100

    Script improvements Make singularity script consistent with standalone version

--- run_colabfold_singularity.sh
+++ run_colabfold_singularity.sh
@@ -9,11 +9,12 @@
 
 set -e
 
+image='/cluster/gjb_lab/jabbott/singularity/colabfold_batch.1.5.2.sif'
+
 usage() {
 	echo "$0 -i /path/to/fasta/file [-c 'colabfold arguments']"
 	exit 1
 }
-
 
 while getopts "i:c:h" opt; do
 	case $opt in
@@ -40,17 +41,26 @@ for arg in "${colabfold_args_list[@]}"; do
 		echo "Should this occur, rerun without --use-gpu-relax"
 		echo
 	fi
-
 done
 
 if [[ -z "$input" ]]; then
 	usage
 fi
 
+if [[ ! -e "$input" ]]; then
+	echo "Specified input file (${input} not found..."
+	exit 1
+fi
+
+# We need to bind the path to the directory containing the input fasta file into the container
+# and also provide the filename...
+input_dir=$(dirname $input)
+fasta_file=$(basename $input)
+
 echo "Hostname: $HOSTNAME"
 echo "GPU: $CUDA_VISIBLE_DEVICES"
-echo "Command line: 'colabfold_batch  ${colabfold_args_list[@]} ${input} colabfold_outputs"
+echo "Command line: colabfold_batch ${colabfold_args_list[@]} ${input} colabfold_outputs"
 
-TF_CPP_MIN_LOG_LEVEL=2
-colabfold_batch ${colabfold_args_list[@]} ${input} colabfold_outputs
-
+export TF_CPP_MIN_LOG_LEVEL=2
+singularity exec --nv -B ${input_dir}:/mnt ${image} \
+	colabfold_batch ${colabfold_args_list[@]} /mnt/${fasta_file} /mnt/colabfold_output
