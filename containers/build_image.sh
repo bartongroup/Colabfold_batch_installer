148b688e79793af7b7f014b59029da70ab0eb077
James Abbott <j.abbott@dundee.ac.uk>
2023-05-25 14:57:23 +0100

    Updates to paths to work in subdir

--- containers/build_image.sh
+++ containers/build_image.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Builds a docker image with versioning extracted from yaml file
 # This uses 'buildx' for a cross-platform build allowing it be be built on MacOS 
 # where we have Docker available, then the image can be transferred to the cluster
@@ -8,8 +10,18 @@
 # Doing a direct singularity build requires sudo access, or can be run on MacOS 
 # in a VM....
 
-version=$(grep "ColabFold.git" ../colabfold_batch.yaml|cut -d@ -f3|sed 's/^v//')
+if [[ ! -e "colabfold_batch.yaml" ]]; then
+	echo "Please run this script from the root of the repository" 
+	echo "i.e. ./containers/$0"
+	exit 1
+fi
+
+version=$(grep "ColabFold.git" colabfold_batch.yaml|cut -d@ -f3|sed 's/^v//')
 echo "Building version ${version}..."
 
-docker buildx build --platform linux/amd64 -t colabfold_batch:${version} --load .
-docker save colabfold_batch:${version} > colabfold_batch.${version}.tar
+docker buildx build --platform linux/amd64 -f containers/Dockerfile -t colabfold_batch:${version} --load .
+docker save colabfold_batch:${version} > containers/colabfold_batch.${version}.tar
+
+echo
+echo "The singularity container can now be built on a linux host using: "
+echo "singularity build colabfold_batch.${version}.sif docker-archive://colabfold_batch.${version}.tar"
